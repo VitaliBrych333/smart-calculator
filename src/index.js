c9906a61c3fbd9c477a3763d7ab87bd9e8520c38
VitaliBrych333 <36643236+VitaliBrych333@users.noreply.github.com>
2018-09-18 22:37:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,27 +4,27 @@ class SmartCalculator {
   }
 
   add(number) {
-    this.initialValue = `${this.initialValue}+${number}`;  // вставка значений выражений в строку
+    this.initialValue = `${this.initialValue} + ${number}`;  // вставка значений выражений в строку
     return this;
   }
 
   subtract(number) {
-    this.initialValue = `${this.initialValue}-${number}`;
+    this.initialValue = `${this.initialValue} - ${number}`;
     return this;
   }
 
   multiply(number) {
-    this.initialValue = `${this.initialValue}*${number}`;
+    this.initialValue = `${this.initialValue} * ${number}`;
     return this;
   }
 
   devide(number) {
-    this.initialValue = `${this.initialValue}/${number}`;
+    this.initialValue = `${this.initialValue} / ${number}`;
     return this;
   }
 
   pow(number) {
-    this.initialValue = `${this.initialValue}**${number}`;
+    this.initialValue = `${this.initialValue} ** ${number}`;
     return this;
   }
 
